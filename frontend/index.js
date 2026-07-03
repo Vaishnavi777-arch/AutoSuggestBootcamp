14588e60d57d76b3b03b605f17a8f5cfe329ca2c
K SRI VAISHNAVI <24wh1a05j2@bvrithyderabad.edu.in>
2026-07-03 19:16:32 +0530

    Completed

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,24 @@
+const users = [
+  {
+    name: "Jane Doe",
+    gender: "Female",
+    image: "./images/jane.png"
+  },
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "./images/john.png"
+  }
+];
+let curUserId = 0;
+function toggle() {
+  if (curUserId === 0) {
+    curUserId = 1;
+  } else {
+    curUserId = 0;
+  }
+  document.getElementById("img").src = users[curUserId].image;
+  document.getElementById("card-name").innerText = users[curUserId].name;
+  document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
 console.log("Hello from JS");
-alert("Hello from JS");
